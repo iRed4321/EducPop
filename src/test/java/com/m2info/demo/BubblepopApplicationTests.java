f08a04f98704fe1774b4a965f41867d47b50fd32
iRed4321 <ired4321@gmail.com>
2023-11-13 22:30:37 +0100

    init

---
+++ src/test/java/com/m2info/demo/BubblepopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2info.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubblepopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
