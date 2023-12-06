06b3237cbafb5275507f2c161cc92435d00be66c
iRed4321 <ired4321@gmail.com>
2023-12-06 21:13:24 +0100

    better

--- src/main/java/com/arwlowloh/bubblepop/CustomerRepository.java
+++ src/main/java/com/arwlowloh/bubblepop/CustomerRepository.java
@@ -1,7 +1,11 @@
 package com.arwlowloh.bubblepop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@RepositoryRestResource
+@CrossOrigin("http://localhost:3000")
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 }
