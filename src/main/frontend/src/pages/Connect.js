f52b2045041e53203ec722981f422cd9cf9f8bcc
alex <=>
2024-01-17 12:55:30 +0100

    Fix connection to database

--- src/main/frontend/src/pages/Connect.js
+++ src/main/frontend/src/pages/Connect.js
@@ -6,23 +6,24 @@ import Logo from "../components/Logo";
 import "../styles/pages/Connect.scss";
 import axios from "../axios.js";
 
+
 import { useState } from 'react';
 
+const Connect = () => {
 
 
-class Connect extends React.Component {
+  let usernameInput = React.createRef();
+  let passwordInput = React.createRef();
+   
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      username: '',
-      password: '',
-    };
-  }
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     console.log("ça change")
-    let formData = this.state;
+    let formData = { ...formData };
     this.setFormData({
       ...formData,
       [e.target.name]: e.target.value,
@@ -30,23 +31,26 @@ class Connect extends React.Component {
   };
 
 
-  async handleLogin(e) {
-    //axios.post("/api/auth/perform_login2").catch(err => console.log(err));
-      try {
-        let formData = this.state;
-        const response = await axios.post("/api/auth/perform_login", formData);
-        console.log(response.data);
-        const token = response.data.token; 
-      } catch (error) {
-        //there is an error
-        console.error('Login failed:', error.message);
-      }
-  }
 
-  render() {
+    const handleLogin = async (e) => {
+        try {
+            e.preventDefault();
+            let username = usernameInput.current.value;
+            let password = passwordInput.current.value;
+            let formData = { };
+            formData.username = username;
+            formData.password = password;
+            const response = await axios.post("/api/auth/perform_login", formData);
+            console.log(response.data);
+            const token = response.data.token; 
+        } catch (error) {
+            //there is an error
+            console.error('Login failed:', error.message);
+        }
+    }
 
     return (
-      <div id="connectPage">
+        <div id="connectPage">
         <div id="connectHeader">
         <Logo />
         <div id='returnHome'>
@@ -55,21 +59,20 @@ class Connect extends React.Component {
         </div>
         </div>
         <div id='connectBody'>
-          <label id="usernameLabel" htmlFor='username'>Nom d'utilisateur</label>
-           <wired-input id="username" name="username" placeholder="Nom d'utilisateur"></wired-input>
-          {/*<input id="username" name="username" value={formData.username} onChange={handleChange} placeholder="Nom d'utilisateur"></input>*/}
-          <label id="passwordLabel" htmlFor="password">Mot de passe</label>
-           <wired-input type='password' name="password" id="password"  placeholder="Mot de passe"></wired-input>
-          {/*<input type='password' name="password" id="password" value={formData.password} onChange={handleChange} placeholder="Mot de passe"></input>
-          <wired-button id="connectButton" onClick={handleLogin}>Se connecter</wired-button >
-          */}
-          <wired-button id="connectButton" >Se connecter</wired-button >
-          <p id="createAccountText">Pas de compte ?<br/>
-          Pas de soucis, s'en créer un, c'est par <Link to={"/signin"}>ici</Link></p>
+        <label id="usernameLabel" htmlFor='username'>Nom d'utilisateur</label>
+        <wired-input id="username" name="username" placeholder="Nom d'utilisateur" ref={usernameInput}></wired-input>
+        {/*<input id="username" name="username" value={formData.username} onChange={handleChange} placeholder="Nom d'utilisateur"></input>*/}
+        <label id="passwordLabel" htmlFor="password">Mot de passe</label>
+        <wired-input type='password' name="password" id="password"  placeholder="Mot de passe" ref={passwordInput}></wired-input>
+        {/*<input type='password' name="password" id="password" value={formData.password} onChange={handleChange} placeholder="Mot de passe"></input>
+            <wired-button id="connectButton" onClick={handleLogin}>Se connecter</wired-button >
+            */}
+        <wired-button id="connectButton" onClick={handleLogin}>Se connecter</wired-button >
+        <p id="createAccountText">Pas de compte ?<br/>
+        Pas de soucis, s'en créer un, c'est par <Link to={"/signin"}>ici</Link></p>
         </div>
-      </div>
+        </div>
     );
-    }
 }
 
 export default Connect;
