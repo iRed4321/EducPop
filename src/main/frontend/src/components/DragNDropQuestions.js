91ea1052824313a205c13b465b0384bcf8e2fc91
alex <=>
2024-01-17 10:54:43 +0100

    Create all the skeleton for the host session

--- src/main/frontend/src/components/DragNDropQuestions.js
+++ src/main/frontend/src/components/DragNDropQuestions.js
@@ -1,1 +1,22 @@
 // Composant qui permet d'afficher une question qu'on y dépose dans une nouvelle fenêtre (pour la rendre visible à l'écran de diaporama)
+import React from 'react';
+
+import "../styles/components/DragNDropQuestions.scss";
+
+import "wired-elements";
+
+class DragNDropQuestions extends React.Component {
+
+    render() {
+        return (
+            <div id="DragNDropQuestionsContainer">
+            <div id="DragNDropQuestionsFrame">
+            <p> Glissez une question ici pour l'afficher
+                dans une nouvelle fenêtre </p>
+            </div>
+            </div>
+        );
+    }
+}
+
+export default DragNDropQuestions;
